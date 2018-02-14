64f9f9d50786704dbf34fcc740ed218e2e63c819
James Cook <jamescook.work@gmail.com>
2018-02-14 17:17:23 -0500

    Complete basic project

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -1,4 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
 using TechJobs.Models;
 
@@ -15,6 +18,48 @@ namespace TechJobs.Controllers
 
         // TODO #1 - Create a Results action method to process 
         // search request and display results
+        public IActionResult Results(string searchTerm, string searchType)
+        {
+            List<Dictionary<string, string>> values = new List<Dictionary<string, string>>();
+            List<Dictionary<string, string>> jobs = JobData.FindAll();
+            if (searchType == "all")
+            {               
+                foreach (Dictionary<string, string> job in jobs)
+                {
+                    foreach (KeyValuePair<string, string> column in job)
+                    {
+                        if (column.Value.IndexOf(searchTerm, System.StringComparison.OrdinalIgnoreCase) >= 0)
+                        {
+                            if (!values.Contains(job))
+                            {
+                                values.Add(job);
+                            }
+                        }
+
+
+                    }
+                }
+                ViewBag.jobs = values;
+                return View("Index");
+            }
+            else
+            {
+                foreach (Dictionary<string, string> job in jobs)
+                {
+                    if (job[searchType].IndexOf(searchTerm, System.StringComparison.OrdinalIgnoreCase) >= 0)
+                    {
+                        if (!values.Contains(job))
+                        {
+                            values.Add(job);
+                        }
+                    }
+                }
+                ViewBag.jobs = values;
+                return View("Index");
+
+            }
+
+        }
 
     }
 }
